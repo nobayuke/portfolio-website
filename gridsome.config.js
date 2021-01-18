6749e017a21d76458b767ae09f560c58b55d0e52
nobayuke <36442757+nobayuke@users.noreply.github.com>
2021-01-18 20:10:27 +0700

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,11 +2,11 @@
 // Learn more: https://gridsome.org/docs/config
 
 module.exports = {
-  siteName: "Portfolio",
-  siteUrl: `https://www.itsnwa.com`,
+  siteName: "Nobayuke Studio",
+  siteUrl: `https://nobayuke.com`,
   host: "0.0.0.0",
-  titleTemplate: "%s - NWA",
-  siteDescription: "Creative technologist",
+  titleTemplate: "%s - Nobayuke Studio",
+  siteDescription: "Studio Kreatif",
   plugins: [
     {
       use: "@gridsome/source-filesystem",
